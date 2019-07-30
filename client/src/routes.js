575ea6debc99f8b1089d6d58785d4a45ab76ca10
Jeremiah Spradlin <sprje02@ca.com>
2019-07-29 22:11:08 -0600

    Client UI testing

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,33 +8,25 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom'
 // import Cards from './components/Cards'
 // import Signup from './components/Signup'
 
-
-
-
-
 import Main from './components/Main'
 
-import ScrollToTop from './components/ScrollTop'
+
+
+// import Main from './components/Main'
+
+// import ScrollToTop from './components/ScrollTop'
 
 export default props => (
     <BrowserRouter>
-        <ScrollToTop>
+        {/* <ScrollToTop> */}
             <Switch>
+                
                 <Route exact path='/' component={Main} />
-
-
-
-
-
-
-
-
-
                 {/* <Route exact path='/dashboard' component={Dashboard} />
                 <Route exact path='/signup' component={Signup} />
                 <Route exact path='/wizard' component={Wizard} />
                 <Route exact path='/cards' component={Cards} /> */}
             </Switch>
-        </ScrollToTop>
+        {/* </ScrollToTop> */}
     </BrowserRouter>
 )
